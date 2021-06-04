a0e522de2ad22e6209304531e1abd164a17f944d
Hong Ooi <hongooi73@gmail.com>
2021-06-04 10:54:10 +1000

    install languageserver

--- install_packages.R
+++ install_packages.R
@@ -1,12 +1,14 @@
 #update.packages(ask=FALSE)
 
 packages <- c(
-	"dataCompareR",
-	"esquisse",
-	"officer",
-	"rvg",
-	"ROCR",
-	"docxtractr"
+    "dataCompareR",
+    "esquisse",
+    "officer",
+    "rvg",
+    "ROCR",
+    "docxtractr",
+    "Microsoft365R",
+    "languageserver"
 )
 
 install.packages(packages)
